da0f0e47aed473616e1894de3c3ad7e80bf02b31
folabia1 <27552121+folabia1@users.noreply.github.com>
2022-04-07 16:27:22 +0100

    Update Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -16,8 +16,7 @@ export function Calculator(props) {
         if (doesStackHoldResult) {
           return [value];
         }
-        if (prevStack.length === 0) {
-          setDoesStackHoldResult(false);
+        if (prevStack.length === 0) { // stack is empty
           return [value];
         }
 
@@ -32,26 +31,39 @@ export function Calculator(props) {
     } else if (value === "_") { // (-) NEGATIVE
       setStack((prevStack) => {
         if (prevStack.length === 0) { // stack is empty
-          setDoesStackHoldResult(false)
-          return ["_"];
-        } else if (!isNaN(prevStack[prevStack.length-1])) { // final stack item is a number
-          return [...prevStack] 
-        } else { // final stack item is an operation or (-)
-          // setDoesStackHoldResult(false)
+          return [value];
+        }
+        
+        if (/^[+\-*/]$/.test(prevStack[prevStack.length-1])) { // final stack item is an operation
           return [...prevStack, value]
+        } else { // final stack item is a number or (-)
+          return prevStack
+        }  
+      })
+    } else if (value === ".") { // DECIMAL POINT
+      setStack((prevStack) => {
+        if (doesStackHoldResult) {
+          return prevStack;
+        }
+        if (prevStack.length === 0) { // stack is empty
+          return [];
+        }
+        if (!isNaN(prevStack[prevStack.length-1]) && Number.isInteger(parseFloat(prevStack[prevStack.length-1]))) { // final stack item is a INTEGER
+          return [...prevStack.slice(0, -1), prevStack[prevStack.length-1] + (value)]
+        } else { // final stack item is an operation or (-)
+          return prevStack
         }  
       })
     } else { // OPERATION
       setStack((prevStack) => {
-        setDoesStackHoldResult(false);
-        if (prevStack.length === 0) {
+        if (prevStack.length === 0) { // stack is empty
           return [];
         } 
-
+        setDoesStackHoldResult(false);
         if (!isNaN(prevStack[prevStack.length-1])) { // final stack item is a number
           return [...prevStack, value]
-        } else { // final stack item is an operation or (-)
-          return [...prevStack]
+        } else { // final stack item is an operation or (-) or decimal point
+          return prevStack
         }
       })
     }
@@ -74,26 +86,27 @@ export function Calculator(props) {
       case "*":
         return parseFloat(num1) * parseFloat(num2);
       case "/":
-        return Math.floor(parseFloat(num1) / parseFloat(num2));
+        return parseFloat(num1) / parseFloat(num2);
       default:
         return null;
     }
   }
   
   function calculate() {
+    if (isNaN(stack[stack.length-1])) {
+      return;
+    }
     let inputNumbers = []
     let inputOperations = []
     stack.forEach((symbol, index) => {index%2===0
       ? inputNumbers.push(symbol)
       : inputOperations.push(symbol)
     })
-    // console.log(inputNumbers, inputOperations);
     let foundIndex;
     let value;
     ["/","*","+","-"].forEach((opType) => {
       foundIndex = inputOperations.findIndex(item => item === opType);
       while (foundIndex !== -1) {
-        console.log(inputNumbers, inputOperations);
         value = simpleOp(opType, inputNumbers[foundIndex], inputNumbers[foundIndex+1]);
         inputNumbers.splice(foundIndex,2,value.toString());
         inputOperations.splice(foundIndex,1);
@@ -107,7 +120,7 @@ export function Calculator(props) {
 
   
   const numbers = ["0","1","2","3","4","5","6","7","8","9"];
-  const operations = ['+','-','*','/']//.concat(['^','(',')']);
+  const operations = ['+','-','*','/',"."]//.concat(['^','(',')']);
   return (
     <div className="Calculator">
       <div className="Numbers">
